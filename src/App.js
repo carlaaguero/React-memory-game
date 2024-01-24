ecde6456d49dbd3637d8944c0b92fe0281255e54
carlaaguero <c.agueromiranda@gmail.com>
2024-01-24 20:17:26 +0100

    Creating the cards, adding the functions to them, and applying the CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState } from 'react';
 import './App.css'
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  {"src": "/img/diamond-1.png"},
+  {"src": "/img/diamond-2.png"},
+  {"src": "/img/diamond-3.png"},
+  {"src": "/img/diamond-4.png"},
+  {"src": "/img/diamond-5.png"},
+  {"src": "/img/diamond-6.png"}
+]
 
 function App() {
+  const [cards, setCards] = useState ([])
+  const [turns, setTurns] = useState (0)
+
+// shuffle cards
+const shuffleCards = () => {
+  const shuffleCards = [...cardImages, ...cardImages]
+  .sort(() => Math.random() - 0.5)
+  .map((card) => ({ ...card, id: Math.random() }))
+
+  setCards(shuffleCards)
+  setTurns(0)
+}
+
+console.log(cards, turns)
+
   return (
     <div className="App">
-      <h1>Magic Match</h1>
-      <button>New Game</button>
+      <h1>Diamond Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SingleCard key={card.id} card={card} />
+        ))}
+      </div>
     </div>
   );
 }
